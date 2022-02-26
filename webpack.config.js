8627e0b6a61419c762bb2f47a327b8c71fc2cc19
sonagi784 <sonagi784@gmail.com>
2022-02-27 03:35:27 +0900

    used vue-cli prerender plugin

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,17 @@ module.exports = {
       '~~': srcPath,
     },
   },
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ['/', '/about'],
+      useRenderEvent: true,
+      onlyProduction: true,
+
+      headless: false, // <- this could also be inside the customRendererConfig
+      customRendererConfig: {
+        args: ['--auto-open-devtools-for-tabs'],
+      },
+    },
+  },
 }
